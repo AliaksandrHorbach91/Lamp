cf42c07d4823166e953b39b43b3354f769393f8f
Aliaksandr Horbach <horbachalex91@gmail.com>
2021-10-13 18:03:49 +0200

    feat: add toggle, opacity lamp

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector('#btn');
+let light = document.querySelector('#light');
+function toggleBtn() {
+    btn.classList.toggle('active');
+    light.classList.toggle('on');
+}
+
+btn.addEventListener('click', toggleBtn)
